a2afb67ddb330ffc9b0bc0c2a37578795805212a
Michelle Poterek <poterekm@gmail.com>
2017-06-19 23:02:11 -0700

    add alert to pizza size if it is not selected

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,18 @@ function Pizza (size, topping) {
   this.topping = topping;
 }
 
+// var sizeCheck = function() {
+//   if (this.size !== "small") && (this.size !== "medium") && (this.size !== "large") && (this.size !== "mondo") {
+//     return false;
+//   }
+// });
+
+console.log(Pizza.size);
+
 Pizza.prototype.calcTotal = function() {
   var total = 0;
   if (this.size === "small") {
     total += 12
-    console.log("hey");
   } else if (this.size === "medium") {
     total += 18
   } else if (this.size === "large") {
@@ -16,8 +23,7 @@ Pizza.prototype.calcTotal = function() {
   } else if (this.size === "mondo") {
     total += 60
   } else {
-    total = 66
-    alert("uhhh... do you want us to clean out our kitchen for you or not? Ya need to pick a size in order for us to make this.")
+    return Pizza.size = undefined;
   }
 
     // Calculate based number of toppings
@@ -38,10 +44,16 @@ $(document).ready(function() {
       userToppings.push($(this).val());
     });
 
-    var newPizza = new Pizza(inputSize, userToppings);
+    var newPizza = new Pizza(inputSize, userToppings, Pizza.calcTotal);
 
+    if (Pizza.size === undefined) {
+      alert("Please enter a size.");
+    }
     $("#user-size").text(newPizza.size);
     $("#user-toppings").text(newPizza.topping + " ,");
     $("#user-total").text("$" + newPizza.calcTotal());
+    // newPizza.calcTotal.val() === false {
+    //   alert("Please enter your size!");
+    // }
   });
 });
